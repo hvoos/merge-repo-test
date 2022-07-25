fcd0d0c9c0a58334f5f21037215088d77aad045e
Heinrich Eberhardt <heberhardt@Macbook-Pro-Black-Series.local>
2022-07-25 12:49:53 -0300

    second merge commit

---
+++ cla/class1.cs
@@ -0,0 +1,7 @@
+public string CallResponse(){
+	return "Hello this is a new response ;)";
+}
+
+public string NewResponseFromMergeResult(){
+	return "New reponse";
+}
---
+++ cla/class2.cs
@@ -0,0 +1,7 @@
+public string FromClass2CallResponse(){
+	return "Hello this is a new response ;)";
+}
+
+public string FromClass2NewResponseFromMergeResult(){
+	return "New reponse";
+}
